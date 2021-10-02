4a766e8675b4abacb0fa4c922bc6d45695ed0e6f
Richard Vlas <vlasrichard@gmail.com>
2021-10-02 22:54:23 +0200

    added test_eda test function

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,3 +1,12 @@
+'''
+This module contains testing functions and logging to verify if the churn_library
+module works properly.
+
+author: Richard Vlas
+date: October 2021
+'''
+import os
+import glob
 import logging
 import churn_library as cl
 
@@ -9,25 +18,44 @@ logging.basicConfig(
 
 
 def test_import(import_data):
+    '''
+    test import_data function
+    '''
+    try:
+        df = import_data("./data/bank_data.csv")
+    except FileNotFoundError as err:
+        logging.error("Testing import_data function: The file wasn't found")
+        raise err
+
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+        logging.info("Testing import_data function: SUCCESS")
+    except AssertionError as err:
+        logging.error("Testing import_data function: The file doesn't appear to have rows or columns")
+        raise err
+
+    return df
+
+
+def test_eda(perform_eda, df):
 	'''
-	test import_data function
+	test perform_eda function
 	'''
-	try:
-		df = import_data("./data/bank_data.csv")
-	except FileNotFoundError as err:
-		logging.error("Testing import_data function: The file wasn't found")
-		raise err
+	path_to_eda_imgs = './images/eda/'
+	perform_eda(df, path_to_eda_imgs)
 
 	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-		logging.info("Testing import_data function: SUCCESS")
+		img_list = [os.path.basename(file_pth) for file_pth in glob.glob(path_to_eda_imgs + "*png")]
+		actual = len(img_list)
+		expected = 7
+		assert actual == expected
+		logging.info("Testing perform_eda function: SUCCESS")
 	except AssertionError as err:
-		logging.error("Testing import_data function: The file doesn't appear to have rows or columns")
+		logging.error("Testing perform_eda function: The perform_eda function did not create correct number of images")
 		raise err
-
-	return df
 
 
 if __name__ == "__main__":
-	data = test_import(cl.import_data)
+    data = test_import(cl.import_data)
+    test_eda(cl.perform_eda, data)
